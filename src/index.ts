7d791bd0a40a676f6cc2aed7a765f56df788fda0
Tim <harvard90873@gmail.com>
2021-07-19 17:58:57 +0200

    ansi green

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,7 @@ const parser = yargs(process.argv.slice(2))
 export const CYAN = "\x1b[36m";
 export const RED = "\x1b[31m";
 export const YELLOW = "\x1b[33m";
+export const GREEN = "\x1b[32m";
 export const RESET = "\x1b[0m";
 export const BOLD = "\x1b[31m";
 
