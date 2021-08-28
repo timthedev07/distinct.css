e10564f58182146e3c5d2c01c7200facb75f4106
Tim <harvard90873@gmail.com>
2021-08-28 19:06:41 +0200

    add another test

--- src/tests/htmlPreprocess.test.ts
+++ src/tests/htmlPreprocess.test.ts
@@ -1,16 +1,36 @@
 import { expect, test, describe } from "@jest/globals";
 import { preProcessRawHTML } from "../utils";
 
-describe("Raw HTML Pre-processing function", () => {
+describe("Raw HTML Preprocessing function", () => {
   test("removes html tag", () => {
-    const res = preProcessRawHTML(
-      `
+    const processedHTML = preProcessRawHTML(`
 <!DOCTYPE html>
 <html>
-</html>
-    `
-    );
-    expect(res.indexOf("<html>")).toBe(-1);
-    expect(res.indexOf("</html>")).toBe(-1);
+</html>`);
+    expect(processedHTML.indexOf("<html>")).toBe(-1);
+    expect(processedHTML.indexOf("</html>")).toBe(-1);
+  });
+
+  test("removes entire head section", () => {
+    const processedHTML0 = preProcessRawHTML(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Some title</title>
+    <meta name="description" content="some dummy html" />
+  </head>
+  <body>
+
+  </body>
+</html>`);
+
+    // head tags
+    expect(processedHTML0.indexOf("<head>")).toBe(-1);
+    expect(processedHTML0.indexOf("</head>")).toBe(-1);
+
+    // content inside
+    expect(processedHTML0.indexOf("<title>")).toBe(-1);
+    expect(processedHTML0.indexOf("</title>")).toBe(-1);
+    expect(processedHTML0.indexOf("<meta")).toBe(-1);
   });
 });
