72d4644cbf117bae62ca89c52453b5593d0d0031
Tim <harvard90873@gmail.com>
2021-08-28 17:14:46 +0200

    helper function left to implement

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,18 @@ import { lstatSync } from "fs";
  * @returns
  */
 export const isDirectory = (path: string) => lstatSync(path).isDirectory();
+
+/**
+ * What I do:
+ *
+ * - Remove <html>, <body> tags along with their closing tags
+ * - Remove <head> section
+ *
+ * So this would result in leaving only the nodes inside of the body tag in the string
+ *
+ * @param rawHTML
+ * @returns
+ */
+export const preProcessRawHTML = (rawHTML: string) => {
+  return "";
+};
