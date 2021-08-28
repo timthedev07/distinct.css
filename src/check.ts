df830d79d5627881e4a9a3df4f07245b2967e51f
Tim <harvard90873@gmail.com>
2021-08-28 23:15:34 +0200

    move some functions to utils.ts

--- src/check.ts
+++ src/check.ts
@@ -1,72 +1,8 @@
 import { CheerioAPI } from "cheerio";
-import { readFileSync } from "fs";
-import { ansi, BOLD, CYAN, GREEN, RED, RESET, YELLOW } from "./constants";
+import { ansi, CYAN, GREEN, RED, YELLOW } from "./constants";
 import { deepCssParser } from "./parser";
-import { PositionInfo, Property, RuleSet } from "./types";
-
-export const showInFile = (
-  position: PositionInfo,
-  pointerPosition?: number
-) => {
-  if (!position || !position.source) {
-    return console.error("Error in unknown file.");
-  }
-
-  const lines = readFileSync(position.source).toString().split("\n");
-
-  const line = lines[position.start!.line! - 1];
-
-  const beforeBeforeInd: number = position.start!.line! - 3;
-  const beforeBefore =
-    beforeBeforeInd > -1
-      ? ` ${beforeBeforeInd + 1} | ` + lines[beforeBeforeInd]
-      : "";
-
-  const beforeInd: number = position.start!.line! - 2;
-  const before =
-    beforeInd > -1 ? ` ${beforeInd + 1} | ` + lines[beforeInd] : "";
-
-  const nextInd: number = position.start!.line!;
-  const next =
-    nextInd < lines.length ? ` ${nextInd + 1} | ` + lines[nextInd] : "";
-
-  // prettier-ignore
-  return `║ ${ansi(position.source!, BOLD)}
-║ ${beforeBefore}
-║ ${before}
-║  ${position.start!.line} | ${YELLOW}${line}${RESET}
-║      ${ansi(`${"~".repeat(pointerPosition || line.length)}${pointerPosition ? "^" : ""}${"~".repeat(pointerPosition || line.length)}`, CYAN)}
-║ ${next}`;
-};
-
-export const reportError = (
-  rules: Array<Property>,
-  text: string = "is duplicated in the following places",
-  selector?: string
-) => {
-  const rule = rules[0];
-
-  const position = rule.position;
-
-  if (!position || !position.source) {
-    return console.error("Error in unknown file.");
-  }
-
-  // prettier-ignore
-  console.log(`
-╔══════════════════════════════════════════════════════════════════════════════════╗
-║                                                                               ═══╩═══
-${showInFile(rule.position, position.start!.column! + rule.property.length + 1)}
-║
-║ ${ansi(`\`${rule.property}: ${rule.value}\``, YELLOW)}${selector ? ` of ${ansi(selector, CYAN)}` : ""} ${text}:
-║
-║
-${rules.slice(1).map(each => showInFile(each.position, (each.position.start!.column! + each.property.length + 1))).join(`\n║\n║ ${"=".repeat(40)}\n║\n`)}
-║                                                                               ═══╦═══
-╚══════════════════════════════════════════════════════════════════════════════════╝
-
-`);
-};
+import { Property, RuleSet } from "./types";
+import { reportError } from "./utils";
 
 export const checkDir = async (
   path: string,
@@ -208,8 +144,11 @@ export const checkCss = (
 };
 
 export const checkUnused = (cssFileContents: RuleSet[], $: CheerioAPI) => {
-  console.log($.html());
   for (const ruleset of cssFileContents) {
-    ruleset;
+    ruleset.selectors.forEach((each) => {
+      // if no such selector can be applied to any HTML elements
+      if ($(each).get().length > 0) {
+      }
+    });
   }
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-import { lstatSync } from "fs";
+import { lstatSync, readFileSync } from "fs";
+import { ansi, BOLD, YELLOW, RESET, CYAN } from "./constants";
+import { PositionInfo, Property } from "./types";
 
 /**
  *
@@ -6,3 +8,67 @@ import { lstatSync } from "fs";
  * @returns
  */
 export const isDirectory = (path: string) => lstatSync(path).isDirectory();
+
+export const showInFile = (
+  position: PositionInfo,
+  pointerPosition?: number
+) => {
+  if (!position || !position.source) {
+    return console.error("Error in unknown file.");
+  }
+
+  const lines = readFileSync(position.source).toString().split("\n");
+
+  const line = lines[position.start!.line! - 1];
+
+  const beforeBeforeInd: number = position.start!.line! - 3;
+  const beforeBefore =
+    beforeBeforeInd > -1
+      ? ` ${beforeBeforeInd + 1} | ` + lines[beforeBeforeInd]
+      : "";
+
+  const beforeInd: number = position.start!.line! - 2;
+  const before =
+    beforeInd > -1 ? ` ${beforeInd + 1} | ` + lines[beforeInd] : "";
+
+  const nextInd: number = position.start!.line!;
+  const next =
+    nextInd < lines.length ? ` ${nextInd + 1} | ` + lines[nextInd] : "";
+
+  // prettier-ignore
+  return `║ ${ansi(position.source!, BOLD)}
+║ ${beforeBefore}
+║ ${before}
+║  ${position.start!.line} | ${YELLOW}${line}${RESET}
+║      ${ansi(`${"~".repeat(pointerPosition || line.length)}${pointerPosition ? "^" : ""}${"~".repeat(pointerPosition || line.length)}`, CYAN)}
+║ ${next}`;
+};
+
+export const reportError = (
+  rules: Array<Property>,
+  text: string = "is duplicated in the following places",
+  selector?: string
+) => {
+  const rule = rules[0];
+
+  const position = rule.position;
+
+  if (!position || !position.source) {
+    return console.error("Error in unknown file.");
+  }
+
+  // prettier-ignore
+  console.log(`
+╔══════════════════════════════════════════════════════════════════════════════════╗
+║                                                                               ═══╩═══
+${showInFile(rule.position, position.start!.column! + rule.property.length + 1)}
+║
+║ ${ansi(`\`${rule.property}: ${rule.value}\``, YELLOW)}${selector ? ` of ${ansi(selector, CYAN)}` : ""} ${text}:
+║
+║
+${rules.slice(1).map(each => showInFile(each.position, (each.position.start!.column! + each.property.length + 1))).join(`\n║\n║ ${"=".repeat(40)}\n║\n`)}
+║                                                                               ═══╦═══
+╚══════════════════════════════════════════════════════════════════════════════════╝
+
+`);
+};
