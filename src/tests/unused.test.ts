10515cc0ac862ccdd101c5682dbc1c9c705f38c5
Tim <harvard90873@gmail.com>
2021-08-29 11:59:03 +0200

    update unused.test.ts
    wrap existing tests in another describe block
    
    clean up test descriptions

--- src/tests/unused.test.ts
+++ src/tests/unused.test.ts
@@ -2,25 +2,38 @@ import { describe, test, expect } from "@jest/globals";
 import { handleUFlagResponse } from "../handlers";
 
 describe("Unused CSS selectors detection", () => {
-  test("recognizes class selectors that are present in the HTML", async () => {
-    const result = await handleUFlagResponse(
-      "/test-data/unused-css-detection/styles/0.css",
-      "/test-data/unused-css-detection/pages/0.html",
-      false
-    );
+  describe("correctly identifies used selectors", () => {
+    test("recognizes class selectors", async () => {
+      const result = await handleUFlagResponse(
+        "/test-data/unused-css-detection/styles/0.css",
+        "/test-data/unused-css-detection/pages/0.html",
+        false
+      );
 
-    // expected to have no unused css
-    expect(result).toHaveLength(0);
-  });
+      // expected to have no unused css
+      expect(result).toHaveLength(0);
+    });
 
-  test("recognizes id selectors that are present in the HTML", async () => {
-    const result = await handleUFlagResponse(
-      "/test-data/unused-css-detection/styles/1.css",
-      "/test-data/unused-css-detection/pages/1.html",
-      false
-    );
+    test("recognizes id selectors", async () => {
+      const result = await handleUFlagResponse(
+        "/test-data/unused-css-detection/styles/1.css",
+        "/test-data/unused-css-detection/pages/1.html",
+        false
+      );
 
-    // expected to have no unused css
-    expect(result).toHaveLength(0);
+      // expected to have no unused css
+      expect(result).toHaveLength(0);
+    });
+
+    test("recognizes element selectors", async () => {
+      const result = await handleUFlagResponse(
+        "/test-data/unused-css-detection/styles/2.css",
+        "/test-data/unused-css-detection/pages/2.html",
+        false
+      );
+
+      // expected to have no unused css
+      expect(result).toHaveLength(0);
+    });
   });
 });
