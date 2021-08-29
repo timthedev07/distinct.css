97a807f75f562bae2cefc15bb1d64dc7c84dbb19
Tim <harvard90873@gmail.com>
2021-08-29 11:06:48 +0200

    more tests

--- src/tests/unused.test.ts
+++ src/tests/unused.test.ts
@@ -2,15 +2,29 @@ import { describe, test, expect } from "@jest/globals";
 import { handleUFlagResponse } from "../handlers";
 
 describe("Unused CSS selectors detection", () => {
-  test("recognizes selectors with classes that are not present in the HTML", async () => {
+  test("recognizes class selectors that are present in the HTML", async () => {
     const result = await handleUFlagResponse(
       "/test-data/unused-css-detection/styles/0.css",
       "/test-data/unused-css-detection/pages/0.html",
       false
     );
-    expect(result).toHaveLength(1);
-    expect(result[0]).toMatchObject({
-      selectors: [".some-other-heading"],
-    });
+
+    // expected to have no unused css
+    expect(result).toHaveLength(0);
+  });
+
+  test("recognizes id selectors that are present in the HTML", async () => {
+    const result = await handleUFlagResponse(
+      "/test-data/unused-css-detection/styles/1.css",
+      "/test-data/unused-css-detection/pages/1.html",
+      false
+    );
+
+    // expected to have no unused css
+    expect(result).toHaveLength(0);
+  });
+
+  test("exit", () => {
+    expect(1 < 2).toEqual(false);
   });
 });
