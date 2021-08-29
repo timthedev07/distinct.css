2087f4160e4bed1e5713720ba5a85a699dd3ca1a
Tim <harvard90873@gmail.com>
2021-08-29 12:45:35 +0200

    another test

--- src/tests/unused.test.ts
+++ src/tests/unused.test.ts
@@ -20,8 +20,6 @@ describe("Unused CSS selectors detection", () => {
         "/test-data/unused-css-detection/pages/1.html",
         false
       );
-
-      // expected to have no unused css
       expect(result).toHaveLength(0);
     });
 
@@ -31,8 +29,6 @@ describe("Unused CSS selectors detection", () => {
         "/test-data/unused-css-detection/pages/2.html",
         false
       );
-
-      // expected to have no unused css
       expect(result).toHaveLength(0);
     });
 
@@ -42,8 +38,15 @@ describe("Unused CSS selectors detection", () => {
         "/test-data/unused-css-detection/pages/3.html",
         false
       );
+      expect(result).toHaveLength(0);
+    });
 
-      // expected to have no unused css
+    test("recognizes descendant elements and immediate children", async () => {
+      const result = await handleUFlagResponse(
+        "/test-data/unused-css-detection/styles/4.css",
+        "/test-data/unused-css-detection/pages/4.html",
+        false
+      );
       expect(result).toHaveLength(0);
     });
   });
