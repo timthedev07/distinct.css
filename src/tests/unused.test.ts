df418e51b4238e3ec54eee017fbd3ce38074eea4
Tim <harvard90873@gmail.com>
2021-08-29 15:21:02 +0200

    dynamic path

--- src/tests/unused.test.ts
+++ src/tests/unused.test.ts
@@ -1,12 +1,15 @@
 import { describe, test, expect } from "@jest/globals";
 import { handleUFlagResponse } from "../handlers";
 
+const BASE_DIR = "/test-data/unused-css-detection";
+const NO_UNUSED_BASE_DIR = BASE_DIR + "/no-unused";
+
 describe("Unused CSS selectors detection", () => {
   describe("correctly identifies used selectors", () => {
     test("recognizes class selectors", async () => {
       const result = await handleUFlagResponse(
-        "/test-data/unused-css-detection/styles/0.css",
-        "/test-data/unused-css-detection/pages/0.html",
+        NO_UNUSED_BASE_DIR + "/styles/0.css",
+        NO_UNUSED_BASE_DIR + "/pages/0.html",
         false
       );
 
@@ -16,8 +19,8 @@ describe("Unused CSS selectors detection", () => {
 
     test("recognizes id selectors", async () => {
       const result = await handleUFlagResponse(
-        "/test-data/unused-css-detection/styles/1.css",
-        "/test-data/unused-css-detection/pages/1.html",
+        NO_UNUSED_BASE_DIR + "/styles/1.css",
+        NO_UNUSED_BASE_DIR + "/pages/1.html",
         false
       );
       expect(result).toHaveLength(0);
@@ -25,8 +28,8 @@ describe("Unused CSS selectors detection", () => {
 
     test("recognizes element selectors", async () => {
       const result = await handleUFlagResponse(
-        "/test-data/unused-css-detection/styles/2.css",
-        "/test-data/unused-css-detection/pages/2.html",
+        NO_UNUSED_BASE_DIR + "/styles/2.css",
+        NO_UNUSED_BASE_DIR + "/pages/2.html",
         false
       );
       expect(result).toHaveLength(0);
@@ -34,8 +37,8 @@ describe("Unused CSS selectors detection", () => {
 
     test("recognizes element.class#id selectors", async () => {
       const result = await handleUFlagResponse(
-        "/test-data/unused-css-detection/styles/3.css",
-        "/test-data/unused-css-detection/pages/3.html",
+        NO_UNUSED_BASE_DIR + "/styles/3.css",
+        NO_UNUSED_BASE_DIR + "/pages/3.html",
         false
       );
       expect(result).toHaveLength(0);
@@ -43,8 +46,8 @@ describe("Unused CSS selectors detection", () => {
 
     test("recognizes descendant elements and immediate children", async () => {
       const result = await handleUFlagResponse(
-        "/test-data/unused-css-detection/styles/4.css",
-        "/test-data/unused-css-detection/pages/4.html",
+        NO_UNUSED_BASE_DIR + "/styles/4.css",
+        NO_UNUSED_BASE_DIR + "/pages/4.html",
         false
       );
       expect(result).toHaveLength(0);
@@ -52,8 +55,8 @@ describe("Unused CSS selectors detection", () => {
 
     test("recognizes attributes", async () => {
       const result = await handleUFlagResponse(
-        "/test-data/unused-css-detection/styles/5.css",
-        "/test-data/unused-css-detection/pages/5.html",
+        NO_UNUSED_BASE_DIR + "/styles/5.css",
+        NO_UNUSED_BASE_DIR + "/pages/5.html",
         false
       );
       expect(result).toHaveLength(0);
