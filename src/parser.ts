30ad116f8b7abd563b029fc11e169e48d1a6cc6f
Tim <harvard90873@gmail.com>
2021-08-29 12:09:53 +0200

    fix: body getting ignore

--- src/parser.ts
+++ src/parser.ts
@@ -165,7 +165,7 @@ export const parseHTML = (path: string) => {
     return null;
   }
 
-  const $ = load(fileContent, null, false);
+  const $ = load(fileContent);
 
   return $;
 };
@@ -216,5 +216,5 @@ export const deepParseHTML = async (
     rawHTMLContents += currentRawHTML;
   }
 
-  return load(rawHTMLContents, null, false);
+  return load(rawHTMLContents);
 };
